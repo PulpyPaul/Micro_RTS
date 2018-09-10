b2585faa32768692ca99b36cf378722028e2fe3a
Jason Bauer <jcb5944@g.rit.edu>
2018-09-09 23:25:34 -0400

    very basic unit spawn and linking some buttons to functions

---
+++ Micro_RTS/Assets/Scripts/SpawnUnit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnUnit : MonoBehaviour {
+    public GameObject unit1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnaUnit()
+    {
+        GameObject Unit = Instantiate(unit1);
+        Unit.transform.position = new Vector3(-3.5f, 0, 0);
+    }
+}

d041e423988316489cfe6d8bb4d0a0cd23aa7a82
Aidan Kaufman <aidan.j.kaufman@gmail.com>
2018-09-10 17:46:58 -0400

    Finished units dying on contact with the enemy base, and enemy base taking damage

--- Micro_RTS/Assets/Scripts/DamageBase.cs
+++ Micro_RTS/Assets/Scripts/DamageBase.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DamageBase : MonoBehaviour {
 
+    private float baseHealth = 100;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +15,18 @@ public class DamageBase : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
+    // On collision, destroy if the tag of the colliding object is unit
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "unit")
+        {
+            baseHealth -= 5;
+
+            Debug.Log("Base Health: " + baseHealth);
+
+            Destroy(other.gameObject);
+        }
+    }
 }

5c749cd9aede15daf2c0f244b9a41e0fa3a4fa00
Kritapas Nimtrakul (RIT Student) <kxn9945@ad.rit.edu>
2018-09-11 21:14:48 -0400

    enemy spawn

--- Micro_RTS/Assets/Scripts/SpawnUnit.cs
+++ Micro_RTS/Assets/Scripts/SpawnUnit.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class SpawnUnit : MonoBehaviour {
     public GameObject rngUnit;
 	public GameObject meleeUnit;
+
+    public GameObject erngUnit;
+    public GameObject emeleeUnit;
+
     public Resources PlayerResources;
 	public Toggle topLane;
 	public Toggle midLane;
@@ -15,18 +19,70 @@ public class SpawnUnit : MonoBehaviour {
 	Vector3 midLoc;
 	Vector3 botLoc;
 
-	// Use this for initialization
-	void Start () {
+
+
+    Random generator = new Random();
+
+    // Use this for initialization
+    void Start () {
         PlayerResources = this.gameObject.GetComponent<Resources>();
-		topLoc = new Vector3 (-3.5f, 3, 0);
-		midLoc = new Vector3 (-3.5f, 0, 0);
-		botLoc = new Vector3 (-3.5f, -3, 0);
-	}
+		topLoc = new Vector3 (-3.5f, 3, 92);
+		midLoc = new Vector3 (-3.5f, 0, 92);
+		botLoc = new Vector3 (-3.5f, -3, 92);
+
+        InvokeRepeating("spawnEnemy", 3.0f, 0.5f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    void spawnEnemy()
+    {
+        float randomF = Random.Range(0.0f, 1.0f);
+        float randomF1 = Random.Range(0.0f, 1.0f);
+        if (randomF < 0.3)
+        {
+            if (randomF1 < 0.5)
+            {
+                GameObject Unit = Instantiate(erngUnit);
+                Unit.transform.position = topLoc + new Vector3(8.74f, 0, 0);
+            }
+            else
+            {
+                GameObject Unit = Instantiate(emeleeUnit);
+                Unit.transform.position = topLoc + new Vector3(8.74f, 0, 0);
+            }
+
+        }
+        else if (randomF < 0.6)
+        {
+            if (randomF1 < 0.5)
+            {
+                GameObject Unit = Instantiate(erngUnit);
+                Unit.transform.position = midLoc + new Vector3(8.74f, 0, 0);
+            }
+            else
+            {
+                GameObject Unit = Instantiate(emeleeUnit);
+                Unit.transform.position = midLoc + new Vector3(8.74f, 0, 0);
+            }
+        }
+        else
+        {
+            if (randomF1 < 0.5)
+            {
+                GameObject Unit = Instantiate(erngUnit);
+                Unit.transform.position = botLoc + new Vector3(8.74f, 0, 0);
+            }
+            else
+            {
+                GameObject Unit = Instantiate(emeleeUnit);
+                Unit.transform.position = botLoc + new Vector3(8.74f, 0, 0);
+            }
+        }
+    }
 
 	public void SpawnRng()
 	{
